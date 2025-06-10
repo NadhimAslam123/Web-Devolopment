1082c08f465c7868c64794d3f23b34c04353c522
NadhimAslam123 <nirooshaaslam71@gmail.com>
2025-06-10 18:56:12 +0530

    Add files via upload

---
+++ HTML/perimeater and area.py
@@ -0,0 +1,8 @@
+import math
+radius = float(input("enter the radius of thr circle"))
+
+area = math.pi * radius ** 2
+perimeter = 2 * math.pi * radius
+
+print("area of the circle :", round (area, 2))
+print("perimeater of the circle :", round (perimeter, 2))
