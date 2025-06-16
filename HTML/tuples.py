adc5dded0ddf3995467a6e969a381d62037b921f
NadhimAslam123 <nirooshaaslam71@gmail.com>
2025-06-16 20:52:04 +0530

    Add files via upload

---
+++ HTML/tuples.py
@@ -0,0 +1,15 @@
+my_tuple = ()
+print(my_tuple)
+
+my_tuple = (1,2,3)
+print(my_tuple)
+
+my_tuple = (1, "hello", 3.4)
+print(my_tuple)
+
+my_tuple = ("mouse", [8,4,6],(1,2,3))
+print(my_tuple)
+
+my_tuple = ('p','e','m','i','t')
+print(my_tuple[0])
+
