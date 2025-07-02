f5cc60d160d41e3110edba2fb724602c2951cac9
NadhimAslam123 <nirooshaaslam71@gmail.com>
2025-07-02 21:29:15 +0530

    Add files via upload

---
+++ HTML/re contents of file.py
@@ -0,0 +1,10 @@
+file = open("Codingal.txt","r")
+Counter = 0
+Content = file.read()
+CoList = Content.split("\n")
+for i in CoList:
+    if i:
+        Counter += 1
+
+print("this is the number of line in the file")
+print(Counter)
