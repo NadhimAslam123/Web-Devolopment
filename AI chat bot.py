2ed8580a3489d5caca1eb730a8aa823c10799e92
NadhimAslam123 <nirooshaaslam71@gmail.com>
2025-09-22 19:46:45 +0530

    Add files via upload

---
+++ AI chat bot.py
@@ -0,0 +1,12 @@
+print("Hello I am AI chat bot. Whats your name?")
+name = input()
+print(f"Thats a cool name {name}")
+print("Whats your favorite sport (Cricket/Football)                Pls Write the first letter of the answer in capital")
+sport = input()
+if sport == "Cricket":
+    print("Thats my favorite sport too , I wish we both choud play together")
+elif sport == "Football":
+    print("Nice but Cricket way better! :)")
+else:
+    print("I think you are not intrested in non of them" )
+print("Thats for today bye!!!")
