fd7ad1abd0b382e432c8bdc702ffee7de99b2603
NadhimAslam123 <nirooshaaslam71@gmail.com>
2025-11-03 20:49:56 +0530

    Add files via upload

---
+++ Resize image acp.py
@@ -0,0 +1,7 @@
+import cv2
+image = cv2.imread('download.jpeg')
+cv2.resizeWindow('Loaded Image', 800, 500)
+cv2.imshow('Loaded Image', image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+print(f"Image Dimensions: {image.shape}")
